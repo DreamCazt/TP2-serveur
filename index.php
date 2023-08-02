7c42cfde49fe79eb25f62fb7085b381a1555cf1c
DominikProvencher <deftonesdom@gmail.com>
2023-08-02 14:35:08 -0400

    Ajout des endpoints put, delete et update.

--- index.php
+++ index.php
@@ -48,6 +48,81 @@ $app->get('/api/posts', function (Request $request, Response $response, array $a
         return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
     }
 });
+// Définition d'une route pour supprimer un article
+$app->delete('/api/post/delete/{id}', function (Request $request, Response $response, $args) use ($db) {
+    // Get the post ID from the URL
+    $id = $args['id'];
+
+    // Instantiation du modèle Post en passant la connexion à la base de données
+    $post = new Post($db);
+
+    // Suppression de l'article
+    if ($post->delete($id)) {
+        // Retour d'une réponse JSON de succès
+        $data = ['message' => 'Post deleted successfully'];
+        $response->getBody()->write(json_encode($data));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+    } else {
+        // Retour d'une réponse JSON d'erreur
+        $data = ['message' => 'Failed to delete post'];
+        $response->getBody()->write(json_encode($data));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+    }
+});
+
+
+// Définition d'une route pour mettre à jour un article
+$app->put('/api/post/update/{id}', function (Request $request, Response $response, $args) use ($db) {
+    // Récupération de l'ID de l'article depuis l'URL
+    $id = $args['id'];
+
+    // Récupération des données mises à jour de l'article depuis le corps de la requête
+    $data = $request->getParsedBody();
+    $title = $data['title'] ?? '';
+    $body = $data['body'] ?? '';
+
+    // Instantiation du modèle Post en passant la connexion à la base de données
+    $post = new Post($db);
+
+    // Update the post
+    if ($post->update($id, $title, $body)) {
+        // Return a success JSON response
+        $data = ['message' => 'Post updated successfully'];
+        $response->getBody()->write(json_encode($data));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+    } else {
+        // Return an error JSON response
+        $data = ['message' => 'Failed to update post'];
+        $response->getBody()->write(json_encode($data));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+    }
+});
+
+$app->patch('/api/post/update_title/{id}', function (Request $request, Response $response, $args) use ($db) {
+    // Get the post ID from the URL
+    $id = $args['id'];
+
+    // Get the updated post data from the request body
+    $data = $request->getParsedBody();
+    $title = $data['title'] ?? '';
+
+
+    // Instantiate the Post model and pass the database connection
+    $post = new Post($db);
+
+    // Update the post
+    if ($post->update_title($id, $title)) {
+        // Return a success JSON response
+        $data = ['message' => 'Post title updated successfully'];
+        $response->getBody()->write(json_encode($data));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+    } else {
+        // Return an error JSON response
+        $data = ['message' => 'Failed to update title of the post'];
+        $response->getBody()->write(json_encode($data));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+    }
+});
 
 //Run Slim app
 $app->run();
